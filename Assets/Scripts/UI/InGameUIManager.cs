ac76ceec76d64653ddc1fd8eb6e24e3dc88d286a
STD\s20227002 <20227002-nishigakitakuya@hcs.ac.jp>
2024-02-05 17:53:54 +0900

    微調整　表示と落下速度

---
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -0,0 +1,17 @@
+/* 制作日 2024/02/05
+*　製作者 ニシガキ
+*　最終更新日 2024/02/05
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class InGameUIManager : MonoBehaviour {
+
+	// プレイヤーのHP量を表すポインターたち
+	private Image[] _playerHPPointers = default;
+
+	// プレイヤーのSP量を表すゲージ
+	private Image _playerSPGage = default;
+
+}
